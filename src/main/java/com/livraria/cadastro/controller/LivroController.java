eb77d309adf30c724534592c83fc64bd568f30cc
unknown <alinedsc1989@gmail.com>
2020-08-17 01:41:08 -0300

    Criado a classe controller com êxito

--- src/main/java/com/livraria/cadastro/controller/LivroController.java
+++ src/main/java/com/livraria/cadastro/controller/LivroController.java
@@ -1,5 +1,60 @@
 package com.livraria.cadastro.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.livraria.cadastro.model.Livro;
+import com.livraria.cadastro.repository.LivroRepository;
+
+@RestController
+@RequestMapping("/api")
 public class LivroController {
+	
+	@Autowired
+	LivroRepository livrosRepository;
+	
+	@GetMapping("listar")
+	public List<Livro> listarLivros(){
+		return livrosRepository.findAll();
+	}
+	
+	@PostMapping("cadastrar")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Livro adicionar(@RequestBody Livro livros) {
+		return livrosRepository.save(livros);
+	}
+	
+	@PutMapping("/atualizar/{livrosId}")
+	public ResponseEntity<Livro> atualizar(@PathVariable long livrosId,
+			@RequestBody Livro livros){
+		if(!livrosRepository.existsById(livrosId)) {
+			return ResponseEntity.notFound().build();
+		}
+		livros.setCodigo(livrosId);
+        livros = livrosRepository.save(livros);
+        return ResponseEntity.ok(livros);
+	}
+	
+	@DeleteMapping("/remover/{livrosId}")
+	public ResponseEntity<Void> remover(@PathVariable long livrosId){
+		if(!livrosRepository.existsById(livrosId)) {
+			return ResponseEntity.notFound().build();
+		}
+        livrosRepository.deleteById(livrosId);
+        return ResponseEntity.noContent().build();		
+	}
+
 
 }
--- src/main/java/com/livraria/cadastro/model/Livro.java
+++ src/main/java/com/livraria/cadastro/model/Livro.java
@@ -2,7 +2,11 @@ package com.livraria.cadastro.model;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.EqualsAndHashCode;
@@ -14,21 +18,27 @@ import lombok.Setter;
 @Getter @Setter @EqualsAndHashCode
 public class Livro {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 	
+	@Column(name="nome_livros")
 	private String nomeLivros;
 	
+	@Column(name="autor")
 	private String autor;
 	
+	@Column(name="editora")
 	private String editora;
 	
+	@Column(name="categoria")
 	private String categoria;
 	
+	@Column(name="data_publicacao")
 	private Date dataPublicacao;
 	
+	@Column(name="preco")
 	private Double preco;
 	
-
-
 
 }
